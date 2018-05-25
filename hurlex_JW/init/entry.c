090c66317c38af9d8adedf339d678341389659ee
norah <wjing931202@163.com>
2018-05-25 23:44:14 +0800

    update chapter6

--- hurlex_JW/init/entry.c
+++ hurlex_JW/init/entry.c
@@ -5,11 +5,12 @@
 int kern_entry()
 {
 	init_debug();
+	init_gdt();
 	
 	console_clear();
 	console_write_color("Hello, OS kernel!\n", rc_black, rc_green);
 	
-	panic("test");
+	//panic("test");
 	
 	return 0;
 }
