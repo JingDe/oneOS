10f406bcbabd7beb064200624bb5764a3ee2e02f
norah <wjing931202@163.com>
2018-05-27 16:40:58 +0800

    update chapter7

--- hurlex_JW/init/entry.c
+++ hurlex_JW/init/entry.c
@@ -6,11 +6,14 @@ int kern_entry()
 {
 	init_debug();
 	init_gdt();
+	init_idt();
 	
 	console_clear();
-	console_write_color("Hello, OS kernel!\n", rc_black, rc_green);
+	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 	
 	//panic("test");
+	asm volatile ("int $0x3");
+	asm volatile ("int $0x4");
 	
 	return 0;
 }
